26e1d45b53b1eac2634ea84377e03b6317ea9cbc
Anne-Marie Mahfouf <annma@kde.org>
2008-01-22 17:42:53 +0000

    backport fix, thanks to Carlo BUG=155476
    svn path=/branches/KDE/4.0/kdebase/workspace/; revision=764838

--- systemsettings/kcmultiwidget.cpp
+++ systemsettings/kcmultiwidget.cpp
@@ -306,8 +306,14 @@ void KCMultiWidget::applyOrRevert(KCModuleProxy * module){
 	}
 }
 
-void KCMultiWidget::slotAboutToShow(KPageWidgetItem* current, KPageWidgetItem* /*before*/)
+void KCMultiWidget::slotAboutToShow(KPageWidgetItem* current, KPageWidgetItem* before)
 {
+	if( before != 0 ) {
+	    QScrollArea *scrollArea = qobject_cast<QScrollArea*>( before->widget() );
+	    KCModuleProxy *module = qobject_cast<KCModuleProxy*>( scrollArea->widget() );
+	    applyOrRevert( module );
+        }
+
 	QWidget* sendingWidget = current->widget();
 	slotAboutToShow(sendingWidget);
 }
@@ -324,9 +330,6 @@ void KCMultiWidget::slotAboutToShow(QWidget *page)
 		return;
 	}
 
-	if( currentModule() != 0 ) {
-		applyOrRevert( currentModule() );
-	}
 	emit ( aboutToShow( module ) );
 
 	int buttons = 0;
