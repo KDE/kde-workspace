cfbe47ed776aad16ba4e025854b24c7ce8415cd5
Riccardo Iaconelli <riccardo@kde.org>
2008-02-16 14:34:58 +0000

    Yay "Show only tasks from the current desktop" is back. As a thank you I can at least polish it up a bit: * task buttons don't disappear for windows on all desktops after   you switch to another desktop (used isOnCurrentDesktop()) * after you move the window to another desktop the task button now   dissapears * changed the setting label a bit, hope it's a bit better now
    svn path=/branches/KDE/4.0/kdebase/workspace/; revision=775716

--- plasma/applets/tasks/tasks.cpp
+++ plasma/applets/tasks/tasks.cpp
@@ -96,6 +96,10 @@ void Tasks::init()
 	connect(TaskManager::TaskManager::self(), SIGNAL(desktopChanged(int)),
             this, SLOT(currentDesktopChanged(int)));
 
+    connect(TaskManager::TaskManager::self(), SIGNAL(windowChanged(TaskPtr)),
+            this, SLOT(removeMovedWindow(TaskPtr)));
+
+
     // add the animator once we're initialized to avoid animating like mad on start up
     _rootTaskGroup->layout()->setAnimator(animator);
 }
@@ -158,7 +162,7 @@ void Tasks::addWindowTask(TaskPtr task)
         return;
     }
 	
-	if (_showOnlyCurrentDesktop && task->desktop() != TaskManager::TaskManager::self()->currentDesktop())
+	if (_showOnlyCurrentDesktop && !task->isOnCurrentDesktop())
 		return;
     WindowTaskItem *item = 0;
     foreach (StartupPtr startup, _startupTaskItems.keys()) {
@@ -216,6 +220,12 @@ void Tasks::currentDesktopChanged( int )
 	removeAllTasks();
 	registerWindowTasks();
 
+}
+
+void Tasks::removeMovedWindow(TaskPtr task)
+{
+    if (_showOnlyCurrentDesktop && !task->isOnCurrentDesktop())
+    removeWindowTask(task);
 }
 
 void Tasks::showConfigurationInterface()
