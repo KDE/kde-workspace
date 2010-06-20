ed395904c784bd703ee39cd42b1fe581c824977b
Jacopo De Simoi <wilderkde@gmail.com>
2010-06-20 17:59:18 +0000

    backport fix for bogus free space value in some cases
    CCBUG:223925
    
    svn path=/branches/KDE/4.4/kdebase/workspace/; revision=1140446

--- plasma/generic/applets/devicenotifier/deviceitem.cpp
+++ plasma/generic/applets/devicenotifier/deviceitem.cpp
@@ -415,6 +415,11 @@ void DeviceItem::setReady()
     }
 }
 
+DeviceItem::State DeviceItem::state() const
+{
+    return m_state;
+}
+
 void DeviceItem::triggerBusyWidget()
 {
     m_mainLayout->removeItem(m_leftActionIcon);
