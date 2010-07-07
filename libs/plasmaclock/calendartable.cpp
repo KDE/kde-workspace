022c11427b5841e22f106a9af0ccf80b8d94a989
Davide Bettio <davide.bettio@kdemail.net>
2010-07-07 12:07:41 +0000

    Backported my previous commit. It will fix a paint bug.
    svn path=/branches/KDE/4.5/kdebase/workspace/; revision=1147114

--- libs/plasmaclock/calendartable.cpp
+++ libs/plasmaclock/calendartable.cpp
@@ -306,6 +306,8 @@ class CalendarTablePrivate
         QMultiHash<int, Plasma::DataEngine::Data> todos;
         QString eventsQuery;
 
+        QPointF lastSeenMousePos;
+
         Ui::calendarConfig calendarConfigUi;
 
         Plasma::Svg *svg;
@@ -396,8 +398,8 @@ void CalendarTable::setDate(const QDate &newDate)
     // now change the date
     d->setDate(newDate);
 
-    d->updateHoveredPainting(QPointF());
-
+    d->updateHoveredPainting(d->lastSeenMousePos);
+    
     if (oldYear != d->selectedYear || oldMonth != d->selectedMonth) {
         populateHolidays();
         populateEvents();
@@ -737,6 +739,8 @@ void CalendarTable::wheelEvent(QGraphicsSceneWheelEvent * event)
 
 void CalendarTable::mousePressEvent(QGraphicsSceneMouseEvent *event)
 {
+    d->lastSeenMousePos = event->pos();
+  
     event->accept();
     QDate date = d->dateFromPoint(event->pos());
     setDate(date);
@@ -752,11 +756,15 @@ void CalendarTable::mouseReleaseEvent(QGraphicsSceneMouseEvent *event)
 {
     Q_UNUSED(event);
 
+    d->lastSeenMousePos = event->pos();
+    
     emit tableClicked();
 }
 
 void CalendarTable::hoverMoveEvent(QGraphicsSceneHoverEvent *event)
 {
+    d->lastSeenMousePos = event->pos();
+  
     d->updateHoveredPainting(event->pos());
 }
 
