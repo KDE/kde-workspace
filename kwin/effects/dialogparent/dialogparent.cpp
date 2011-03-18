26a5f4dc3ec42616a13eee4f0112a22b6361c241
Thomas Lübking <thomas.luebking@gmail.com>
2011-03-18 23:20:09 +0100

    fix dialogparent implementation
    CCBUG: 267349

--- kwin/effects/dialogparent/dialogparent.cpp
+++ kwin/effects/dialogparent/dialogparent.cpp
@@ -3,6 +3,7 @@
  This file is part of the KDE project.
 
 Copyright (C) 2006 Rivo Laks <rivolaks@hot.ee>
+Copyright (C) 2011 Thomas Lübking <thomas.luebking@web.de>
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -23,92 +24,73 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 namespace KWin
 {
 
-KWIN_EFFECT( dialogparent, DialogParentEffect )
+KWIN_EFFECT(dialogparent, DialogParentEffect)
 
 DialogParentEffect::DialogParentEffect()
-    {
-    reconfigure( ReconfigureAll );
-    }
+{
+    reconfigure(ReconfigureAll);
+}
 
-void DialogParentEffect::reconfigure( ReconfigureFlags )
-    {
+void DialogParentEffect::reconfigure(ReconfigureFlags)
+{
     // How long does it take for the effect to get it's full strength (in ms)
-    changeTime = animationTime( 200 );
-    }
+    changeTime = animationTime(300);
+}
 
-void DialogParentEffect::prePaintWindow( EffectWindow* w, WindowPrePaintData& data, int time )
-    {
-    // Check if this window has a modal dialog and change the window's
-    //  effect's strength accordingly
-    bool hasDialog = w->findModal() != NULL;
-    if( hasDialog )
-        {
-        // Increase effect strength of this window
-        effectStrength[w] = qMin(1.0, effectStrength[w] + time/changeTime);
+void DialogParentEffect::prePaintWindow(EffectWindow* w, WindowPrePaintData& data, int time)
+{
+    QMap<EffectWindow*, float>::iterator it = effectStrength.find(w);
+    if (it != effectStrength.end()) {
+        if (!w->findModal()) {
+            *it -= time/changeTime;
+            if (*it <= 0.0f)
+                effectStrength.erase(it);
         }
-    else
-        {
-        effectStrength[w] = qMax(0.0, effectStrength[w] - time/changeTime);
-        }
-
-    // Call the next effect
-    effects->prePaintWindow( w, data, time );
+        else if (*it < 1.0f)
+            *it = qMin(1.0f, *it + time/changeTime);
     }
+    effects->prePaintWindow(w, data, time);
+}
 
-void DialogParentEffect::paintWindow( EffectWindow* w, int mask, QRegion region, WindowPaintData& data )
-    {
-    double s = effectStrength[w];
-    if(s > 0.0f)
-        {
-        // Brightness will be within [1.0; 0.6]
-        data.brightness *= (1.0 - s * 0.4);
-        // Saturation within [1.0; 0.4]
-        data.saturation *= (1.0 - s * 0.6);
-        }
-
-    // Call the next effect.
-    effects->paintWindow( w, mask, region, data );
+void DialogParentEffect::paintWindow(EffectWindow* w, int mask, QRegion region, WindowPaintData& data)
+{
+    const float s = effectStrength.value(w, 0.0);
+    if (s > 0.0f) {
+        data.brightness *= (1.0f - 0.4*s); // [1.0; 0.6]
+        data.saturation *= (1.0f - 0.6*s); // [1.0; 0.4]
     }
+    effects->paintWindow(w, mask, region, data);
+}
 
-void DialogParentEffect::postPaintWindow( EffectWindow* w )
-    {
-    double s = effectStrength[w];
+void DialogParentEffect::postPaintWindow(EffectWindow* w)
+{
+    const float s = effectStrength.value(w, 0.0);
+    if (s > 0.0f && s < 1.0f) // not done yet
+        w->addRepaintFull();
 
-    // If strength is between 0 and 1, the effect is still in progress and the
-    //  window has to be repainted during the next pass
-    if( s > 0.0 && s < 1.0 )
-        w->addRepaintFull(); // trigger next animation repaint
-
-    // Call the next effect.
     effects->postPaintWindow( w );
-    }
+}
 
-void DialogParentEffect::windowActivated( EffectWindow* w )
-    {
-    // If this window is a dialog, we need to repaint it's parent window, so
-    //  that the effect could be run for it
-    // Set the window to be faded (or NULL if no window is active).
-    if( w && w->isModal() )
-        {
-        // w is a modal dialog
+void DialogParentEffect::windowActivated(EffectWindow* w)
+{
+    if (w && w->isModal()) {
         EffectWindowList mainwindows = w->mainWindows();
-        foreach( EffectWindow* parent, mainwindows )
+        foreach (EffectWindow* parent, mainwindows) {
+            if (!effectStrength.contains(parent))
+                effectStrength[parent] = 0.0;
             parent->addRepaintFull();
         }
     }
+}
 
-void DialogParentEffect::windowClosed( EffectWindow* w )
-    {
-    // If this window is a dialog, we need to repaint it's parent window, so
-    //  that the effect could be run for it
-    // Set the window to be faded (or NULL if no window is active).
-    if ( w && w->isModal() )
-        {
-        // w is a modal dialog
+void DialogParentEffect::windowClosed(EffectWindow* w)
+{
+    if (w && w->isModal()) {
         EffectWindowList mainwindows = w->mainWindows();
-        foreach( EffectWindow* parent, mainwindows )
-            parent->addRepaintFull();
-        }
+        foreach(EffectWindow* parent, mainwindows)
+            parent->addRepaintFull(); // brighten up
     }
+    effectStrength.remove(w);
+}
 
 } // namespace
