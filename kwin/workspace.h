6a07efdd744b47d102c5093613ee1349284fa8ef
Alex Fiestas <afiestas@kde.org>
2011-07-24 22:56:55 +0200

    Try to call desktopRezie once intead of doing it in all XRandR events.
    When a screen is disconnected XRandR emits 3 events instead of just one:
    1-Disconnected screen resized
    2-Disconnected screen moved
    3-Disconnected screen removed
    
    Before this commit we were calling desktopResize on each event which
    between other things restart the composite (not necessarily the faster
    thing to do...).
    
    So, in order to be able to call desktopResize only once, now we're
    handling individually each event, when one of them happens a QTimer is
    started/restarted on each event and desktopResized is called owhen
    QTimer.timeout
    
        The current interval is 100msec
    (cherry picked from commit 6f891693405d695f73231d8d04864389aa47c0b2)

--- kwin/workspace.h
+++ kwin/workspace.h
@@ -55,6 +55,10 @@ class KStartupInfoData;
 class QSlider;
 class QPushButton;
 
+namespace Kephal
+{
+    class Screen;
+}
 namespace KWin
 {
 
@@ -704,6 +708,11 @@ private slots:
     void clientPopupActivated(QAction*);
     void configureWM();
     void desktopResized();
+    void screenChangeTimeout();
+    void screenAdded(Kephal::Screen*);
+    void screenRemoved(int);
+    void screenResized(Kephal::Screen*, QSize, QSize);
+    void screenMoved(Kephal::Screen*, QPoint, QPoint);
     void slotUpdateToolWindows();
     void lostTopMenuSelection();
     void lostTopMenuOwner();
@@ -833,7 +842,6 @@ private:
 
     QMenu* clientPopup();
     void closeActivePopup();
-
     void updateClientArea(bool force);
 
     bool windowRepaintsPending() const;
@@ -856,6 +864,7 @@ private:
     QList<Rules*> rules;
     KXMessages temporaryRulesMessages;
     QTimer rulesUpdatedTimer;
+    QTimer screenChangedTimer;
     bool rules_updates_disabled;
     static const char* windowTypeToTxt(NET::WindowType type);
     static NET::WindowType txtToWindowType(const char* txt);
