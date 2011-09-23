dbe57396853f9995a9b3d7ab5d55f88fc730512a
Marco Martin <notmart@gmail.com>
2011-09-23 17:55:00 +0200

    fix close button side for present windows effect BUG: 262543
    pint desktop as background when including desktop in switcher
    BUG: 262137
    
    zoom windows as hover/selection indicaton (1/8 of the screen or 105%)
    BUG: 215348
    CCBUG: 175521
    
    no closer on "show desktop" desktop
    show closer immediately but have it disabled for a short time to allow the user realize it
    
    REVIEW: 101318
    
    Conflicts:
    
    	kwin/effects.h

--- kwin/effects.cpp
+++ kwin/effects.cpp
@@ -1191,6 +1191,17 @@ EffectFrame* EffectsHandlerImpl::effectFrame(EffectFrameStyle style, bool static
     return new EffectFrameImpl(style, staticSize, position, alignment);
 }
 
+
+QVariant EffectsHandlerImpl::kwinOption(KWinOption kwopt)
+{
+    switch (kwopt)
+    {
+        case CloseButtonCorner:
+            return Workspace::self()->decorationCloseButtonCorner();
+    }
+    return QVariant(); // an invalid one
+}
+
 void EffectsHandlerImpl::slotShowOutline(const QRect& geometry)
 {
     emit showOutline(geometry);
