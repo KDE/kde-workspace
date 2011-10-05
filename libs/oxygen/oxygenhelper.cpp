d87e70e1bb106a35cde1572883831635b9173628
Hugo Pereira Da Costa <hugo@oxygen-icons.org>
2011-10-05 23:00:38 +0200

    Make sure calculated shadow color has the same alpha channel as the input.

--- libs/oxygen/oxygenhelper.cpp
+++ libs/oxygen/oxygenhelper.cpp
@@ -434,6 +434,11 @@ namespace Oxygen
                 KColorUtils::mix( Qt::black, color, color.alphaF() ),
                 KColorScheme::ShadowShade,
                 _contrast ) );
+
+            // make sure shadow color has the same alpha channel as the input
+            out->setAlpha( color.alpha() );
+
+            // insert in cache
             _shadowColorCache.insert( key, out );
         }
 
