b9aa555ddf0b8af529afe51fb9370d6f0c0329c0
Lukas Tinkl <lukas@kde.org>
2011-11-25 04:08:50 +0100

    update the plasmoid slider when the brightness changes (eg. via keyboard shortcut)

--- plasma/generic/applets/battery/battery.cpp
+++ plasma/generic/applets/battery/battery.cpp
@@ -163,7 +163,7 @@ void Battery::init()
         initPopupWidget();
         // let's show a brightness OSD
         QDBusConnection::sessionBus().connect("org.kde.Solid.PowerManagement", "/org/kde/Solid/PowerManagement", "org.kde.Solid.PowerManagement",
-                                              "brightnessChanged", this, SLOT(updateSlider(float)));
+                                              "brightnessChanged", this, SLOT(updateSlider(int)));
     }
 
     if (m_acAdapterPlugged) {
@@ -443,11 +443,11 @@ void Battery::updateSlider()
     updateSlider(reply.value());
 }
 
-void Battery::updateSlider(const float brightness)
+void Battery::updateSlider(int brightness)
 {
-    if (m_brightnessSlider->value() != (int)brightness) {
+    if (m_brightnessSlider->value() != brightness) {
         m_ignoreBrightnessChange = true;
-        m_brightnessSlider->setValue((int) brightness);
+        m_brightnessSlider->setValue(brightness);
         m_ignoreBrightnessChange = false;
     }
 }
