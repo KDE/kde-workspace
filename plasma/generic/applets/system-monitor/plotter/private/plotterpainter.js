b15792fb583473a93a370345bdefff3ff3019ddb
Shaun Reich <shaun.reich@kdemail.net>
2012-02-15 16:03:42 -0500

    in the tooltip thingy, display the *true* value
    i.e. height - graphpadding - hovertext.y

--- plasma/generic/applets/system-monitor/plotter/private/plotterpainter.js
+++ plasma/generic/applets/system-monitor/plotter/private/plotterpainter.js
@@ -132,15 +132,6 @@ function init(width, height)
     // TODO: find a scalar, mostly for vertSpace
     horizSpace = 10;
     vertSpace  = 1 //height - (graphPadding * (points.length));
-
-    addSample(0);
-    addSample(0);
-    addSample(0);
-    addSample(0);
-    addSample(100);
-    addSample(100);
-    addSample(100);
-    addSample(100);
 }
 
 /**
@@ -220,7 +211,7 @@ function paint(canvas, context)
             context.strokeWidth = 2;
             context.fillStyle = "rgba(50, 0, 0, 1)";
 
-            context.text(hoverText.x, hoverText.x, hoverText.y + 10);
+            context.text(height - graphPadding - hoverText.y, hoverText.x, hoverText.y + 10);
             context.closePath();
             context.stroke();
             context.fill();
