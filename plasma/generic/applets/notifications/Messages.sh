6eeccccca3a5b76244b67b1b47cb8273dbf1c7d5
Marco Martin <notmart@gmail.com>
2012-09-10 14:14:58 +0200

    proper plugin name

--- plasma/generic/applets/notifications/Messages.sh
+++ plasma/generic/applets/notifications/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
-$XGETTEXT `find . -name \*.qml` -L Java -o $podir/plasma_applet_org.kde.active.notifications.pot
+$XGETTEXT `find . -name \*.qml` -L Java -o $podir/plasma_applet_org.kde.notifications.pot
 rm -f rc.cpp
