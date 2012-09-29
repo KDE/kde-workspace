4606e3b82d4f4a3f2478390e26cc135854841f8f
Martin Gräßlin <mgraesslin@kde.org>
2012-09-29 13:19:35 +0200

    Provide OpenGL over Egl
    The Egl backend is decoupled from the OpenGL ES build option which makes
    it possible to use it as a replacement for glx.
    
    To make this possible a new build flag is added when egl is available at
    compile time and any egl specific code is now ifdefed with this flag
    instead of the gles flag. In addition at runtime a windowing system enum
    value is passed to the various detect methods to have egl/glx specific
    detection for e.g. function pointer resolving.
    
    By default egl is used if compiled with OpenGL ES, otherwise glx is used.
    But in the non-gles case the windowing system can be selected through the
    new environment variable KWIN_OPENGL_INTERFACE. Setting this variable to
    "egl" the EglOnXBackend is used.
    
    REVIEW: 106632

--- cmake/modules/FindOpenGLES.cmake
+++ cmake/modules/FindOpenGLES.cmake
@@ -2,6 +2,7 @@
 # Once done this will define
 #  
 #  OPENGLES_FOUND           - system has OpenGLES and EGL
+#  OPENGL_EGL_FOUND         - system has EGL
 #  OPENGLES_INCLUDE_DIR     - the GLES include directory
 #  OPENGLES_LIBRARY	    - the GLES library
 #  OPENGLES_EGL_INCLUDE_DIR - the EGL include directory
@@ -35,21 +36,10 @@ FIND_LIBRARY(OPENGLES_EGL_LIBRARY
           /usr/lib
 )
 
-# On Unix OpenGL most certainly always requires X11.
-# Feel free to tighten up these conditions if you don't 
-# think this is always true.
-# It's not true on OSX.
-
-IF(OPENGLES_LIBRARY)
-  IF(NOT X11_FOUND)
-    INCLUDE(FindX11)
-  ENDIF(NOT X11_FOUND)
-  IF(X11_FOUND)
-    IF(NOT APPLE)
-      SET(OPENGLES_LIBRARIES ${X11_LIBRARIES})
-    ENDIF(NOT APPLE)
-  ENDIF(X11_FOUND)
-ENDIF(OPENGLES_LIBRARY)
+SET(OPENGL_EGL_FOUND "NO")
+IF(OPENGLES_EGL_LIBRARY AND OPENGLES_EGL_INCLUDE_DIR)
+    SET(OPENGL_EGL_FOUND "YES")
+ENDIF()
 
 SET(OPENGLES_FOUND "NO")
 IF(OPENGLES_LIBRARY AND OPENGLES_INCLUDE_DIR AND
