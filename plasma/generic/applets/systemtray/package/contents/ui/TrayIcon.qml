b46714937e2a3845501bd191cfd787c24643f531
Dmitry Ashkadov <dmitry.ashkadov@gmail.com>
2012-10-09 11:07:00 +0400

    License has been changed to GPLv2+ (C++) and LGPLv2+ (QML, JS)

--- plasma/generic/applets/systemtray/package/contents/ui/TrayIcon.qml
+++ plasma/generic/applets/systemtray/package/contents/ui/TrayIcon.qml
@@ -2,20 +2,20 @@
  * KDE System Tray (Plasmoid)
  *
  * Copyright (C) 2012 ROSA  <support@rosalab.ru>
- * License: GPLv3+
+ * License: LGPLv2+
  * Authors: Dmitry Ashkadov <dmitry.ashkadov@rosalab.ru>
  *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 3,
- * or (at your option) any later version.
+ * it under the terms of the GNU Library General Public License as
+ * published by the Free Software Foundation; either version 2 or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details
  *
- * You should have received a copy of the GNU General Public
+ * You should have received a copy of the GNU Library General Public
  * License along with this program; if not, write to the
  * Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
--- plasma/generic/applets/systemtray/ui/widgetitem.h
+++ plasma/generic/applets/systemtray/ui/widgetitem.h
@@ -1,23 +1,24 @@
 /***********************************************************************************************************************
- * System Tray (KDE Plasmoid)
- * Copyright (C) 2011-2012 ROSA  <support@rosalab.ru>
- * License: GPLv3+
+ * KDE System Tray (Plasmoid)
+ *
+ * Copyright (C) 2012 ROSA  <support@rosalab.ru>
+ * License: GPLv2+
  * Authors: Dmitry Ashkadov <dmitry.ashkadov@rosalab.ru>
  *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 3,
- * or (at your option) any later version.
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public
- * License along with this program; if not, write to the
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the
  * Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .
  **********************************************************************************************************************/
 
 
