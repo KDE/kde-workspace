29ebea26dc4d22e2865ead4f6c2f6c4c126fb968
Cedric Bellegarde <gnumdk@gmail.com>
2012-12-26 11:06:19 +0100

    Fix regression: do not try to cache importer on window registering to avoid freeze with kmix

--- appmenu/menuimporter.h
+++ appmenu/menuimporter.h
@@ -73,7 +73,6 @@ public:
     WId recursiveMenuId(WId id);
 
 Q_SIGNALS:
-    // Empty service means that we do not need to update importer
     void WindowRegistered(WId id, const QString& service, const QDBusObjectPath&);
     void WindowUnregistered(WId id);
 
