KDE/kde-workspace

commit f443382388363a6e5b8e12e7154f0fabdbe54e8a

Remove hidden synchronous D-Bus calls from mpris2 plasma dataengine
Using QDBusInterface (rather than a generated subclass of
QDBusAbstractInterface) causes Introspect() to be called synchronously
on construction.  So we use generated interfaces instead.

This has the added bonus of type-checked method calls and
signature-checked signal/slot connections.

plasma/generic/dataengines/mpris2/playercontainer.cpp
@@ -19,10 +19,11 @@
 
 #include "playercontainer.h"
 
+#include <dbusproperties.h>
+#include <mprisplayer.h>
+#include <mprisroot.h>
+
 #define MPRIS2_PATH "/org/mpris/MediaPlayer2"
-#define DBUS_PROPS_IFACE "org.freedesktop.DBus.Properties"
-#define MPRIS2_IFACE "org.mpris.MediaPlayer2"
-#define MPRIS2_PLAYER_IFACE "org.mpris.MediaPlayer2.Player"
 #define POS_UPD_STRING "Position last updated (UTC)"
 
 #include <KDebug>
@@ -100,25 +101,23 @@ PlayerContainer::PlayerContainer(const QString& busAddress, QObject* parent)
     Q_ASSERT(!busAddress.isEmpty());
     Q_ASSERT(busAddress.startsWith(QLatin1String("org.mpris.MediaPlayer2.")));
 
-    m_propsIface = new QDBusInterface(busAddress,
-            MPRIS2_PATH, DBUS_PROPS_IFACE,
+    m_propsIface = new OrgFreedesktopDBusPropertiesInterface(
+            busAddress, MPRIS2_PATH,
             QDBusConnection::sessionBus(), this);
 
-    QDBusConnection::sessionBus().connect(
-            busAddress,
-            MPRIS2_PATH,
-            DBUS_PROPS_IFACE,
-            "PropertiesChanged", /* signature, */
-            this,
-            SLOT(propertiesChanged(QString,QVariantMap,QStringList)));
+    m_playerIface = new OrgMprisMediaPlayer2PlayerInterface(
+            busAddress, MPRIS2_PATH,
+            QDBusConnection::sessionBus(), this);
 
-    QDBusConnection::sessionBus().connect(
-            busAddress,
-            MPRIS2_PATH,
-            MPRIS2_PLAYER_IFACE,
-            "Seeked", /* signature, */
-            this,
-            SLOT(seeked(qint64)));
+    m_rootIface = new OrgMprisMediaPlayer2Interface(
+            busAddress, MPRIS2_PATH,
+            QDBusConnection::sessionBus(), this);
+
+    connect(m_propsIface, SIGNAL(PropertiesChanged(QString,QVariantMap,QStringList)),
+            this,         SLOT(propertiesChanged(QString,QVariantMap,QStringList)));
+
+    connect(m_playerIface, SIGNAL(Seeked(qlonglong)),
+            this,          SLOT(seeked(qint64)));
 
     refresh();
 }
@@ -129,13 +128,13 @@ void PlayerContainer::refresh()
     // wrong order (eg: a stale GetAll response overwriting a more recent value
     // from a PropertiesChanged signal) due to D-Bus message ordering guarantees.
 
-    QDBusPendingCall async = m_propsIface->asyncCall("GetAll", MPRIS2_IFACE);
+    QDBusPendingCall async = m_propsIface->GetAll(OrgMprisMediaPlayer2Interface::staticInterfaceName());
     QDBusPendingCallWatcher *watcher = new QDBusPendingCallWatcher(async, this);
     connect(watcher, SIGNAL(finished(QDBusPendingCallWatcher*)),
             this,    SLOT(getPropsFinished(QDBusPendingCallWatcher*)));
     ++m_fetchesPending;
 
-    async = m_propsIface->asyncCall("GetAll", MPRIS2_PLAYER_IFACE);
+    async = m_propsIface->GetAll(OrgMprisMediaPlayer2PlayerInterface::staticInterfaceName());
     watcher = new QDBusPendingCallWatcher(async, this);
     connect(watcher, SIGNAL(finished(QDBusPendingCallWatcher*)),
             this,    SLOT(getPropsFinished(QDBusPendingCallWatcher*)));
@@ -280,7 +279,9 @@ void PlayerContainer::getPropsFinished(QDBusPendingCallWatcher* watcher)
     }
 
     if (propsReply.isError()) {
-        kWarning() << m_dbusAddress << "does not implement " DBUS_PROPS_IFACE " correctly";
+        kWarning() << m_dbusAddress << "does not implement"
+            << OrgFreedesktopDBusPropertiesInterface::staticInterfaceName()
+            << "correctly";
         kDebug() << "Error message was" << propsReply.error().name() << propsReply.error().message();
         m_fetchesPending = 0;
         emit initialFetchFailed(this);
@@ -298,7 +299,7 @@ void PlayerContainer::getPropsFinished(QDBusPendingCallWatcher* watcher)
 
 void PlayerContainer::updatePosition()
 {
-    QDBusPendingCall async = m_propsIface->asyncCall("Get", MPRIS2_PLAYER_IFACE, "Position");
+    QDBusPendingCall async = m_propsIface->Get(OrgMprisMediaPlayer2PlayerInterface::staticInterfaceName(), "Position");
     QDBusPendingCallWatcher *watcher = new QDBusPendingCallWatcher(async, this);
     connect(watcher, SIGNAL(finished(QDBusPendingCallWatcher*)),
             this,    SLOT(getPositionFinished(QDBusPendingCallWatcher*)));
@@ -310,7 +311,9 @@ void PlayerContainer::getPositionFinished(QDBusPendingCallWatcher* watcher)
     watcher->deleteLater();
 
     if (propsReply.isError()) {
-        kWarning() << m_dbusAddress << "does not implement " DBUS_PROPS_IFACE " correctly";
+        kWarning() << m_dbusAddress << "does not implement"
+            << OrgFreedesktopDBusPropertiesInterface::staticInterfaceName()
+            << "correctly";
         kDebug() << "Error message was" << propsReply.error().name() << propsReply.error().message();
         return;
     }

plasma/generic/dataengines/mpris2/playercontainer.h
@@ -23,8 +23,10 @@
 #include <Plasma/DataContainer>
 #include <QFlags>
 
+class OrgFreedesktopDBusPropertiesInterface;
+class OrgMprisMediaPlayer2Interface;
+class OrgMprisMediaPlayer2PlayerInterface;
 class PlayerControl;
-class QDBusInterface;
 class QDBusPendingCallWatcher;
 
 class PlayerContainer : public Plasma::DataContainer
@@ -35,7 +37,9 @@ public:
     explicit PlayerContainer(const QString& busAddress, QObject* parent = 0);
 
     QString dbusAddress() const { return m_dbusAddress; }
-    QDBusInterface* propertiesInterface() const { return m_propsIface; }
+    OrgFreedesktopDBusPropertiesInterface* propertiesInterface() const { return m_propsIface; }
+    OrgMprisMediaPlayer2Interface* rootInterface() const { return m_rootIface; }
+    OrgMprisMediaPlayer2PlayerInterface* playerInterface() const { return m_playerIface; }
 
     enum Cap {
         NoCaps           = 0,
@@ -81,11 +85,13 @@ private:
     void updatePosition();
     void recalculatePosition();
 
-    Caps            m_caps;
-    int             m_fetchesPending;
-    QString         m_dbusAddress;
-    QDBusInterface *m_propsIface;
-    double          m_currentRate;
+    Caps                                   m_caps;
+    int                                    m_fetchesPending;
+    QString                                m_dbusAddress;
+    OrgFreedesktopDBusPropertiesInterface *m_propsIface;
+    OrgMprisMediaPlayer2Interface         *m_rootIface;
+    OrgMprisMediaPlayer2PlayerInterface   *m_playerIface;
+    double                                 m_currentRate;
 };
 
 Q_DECLARE_OPERATORS_FOR_FLAGS(PlayerContainer::Caps)

plasma/generic/dataengines/mpris2/CMakeLists.txt
@@ -11,6 +11,16 @@ set(mpris2_engine_SRCS
     playercontainer.cpp
 )
 
+set_source_files_properties(
+   org.freedesktop.DBus.Properties.xml
+   org.mpris.MediaPlayer2.Player.xml
+   org.mpris.MediaPlayer2.xml
+   PROPERTIES
+   NO_NAMESPACE ON)
+qt4_add_dbus_interface(mpris2_engine_SRCS org.freedesktop.DBus.Properties.xml dbusproperties)
+qt4_add_dbus_interface(mpris2_engine_SRCS org.mpris.MediaPlayer2.Player.xml mprisplayer)
+qt4_add_dbus_interface(mpris2_engine_SRCS org.mpris.MediaPlayer2.xml mprisroot)
+
 kde4_add_plugin(plasma_engine_mpris2 ${mpris2_engine_SRCS})
 target_link_libraries(plasma_engine_mpris2
    ${KDE4_PLASMA_LIBS}

plasma/generic/dataengines/mpris2/mpris2engine.cpp
@@ -20,7 +20,6 @@
 
 #include <QDBusConnection>
 #include <QDBusConnectionInterface>
-#include <QDBusInterface>
 #include <QDBusServiceWatcher>
 #include <QDBusPendingCallWatcher>
 #include <QDBusPendingReply>

plasma/generic/dataengines/mpris2/org.freedesktop.DBus.Properties.xml
@@ -1,0 +1,27 @@
+<?xml version="1.0" ?>
+<node>
+  <interface name="org.freedesktop.DBus.Properties">
+    <method name="Get">
+      <arg type="s" name="interface_name" direction="in"/>
+      <arg type="s" name="property_name" direction="in"/>
+      <arg type="v" name="value" direction="out"/>
+    </method>
+    <method name="GetAll">
+      <arg type="s" name="interface_name" direction="in"/>
+      <arg type="a{sv}" name="properties" direction="out"/>
+      <annotation name="org.qtproject.QtDBus.QtTypeName.Out0" value="QVariantMap"/>
+    </method>
+    <method name="Set">
+      <arg type="s" name="interface_name" direction="in"/>
+      <arg type="s" name="property_name" direction="in"/>
+      <arg type="v" name="value" direction="in"/>
+    </method>
+    <signal name="PropertiesChanged">
+      <arg type="s" name="interface_name"/>
+      <arg type="a{sv}" name="changed_properties"/>
+      <arg type="as" name="invalidated_properties"/>
+      <annotation name="org.qtproject.QtDBus.QtTypeName.In1" value="QVariantMap"/>
+    </signal>
+  </interface>
+</node>
+<!-- vim:set sw=2 sts=2 et ft=xml: -->

plasma/generic/dataengines/mpris2/org.mpris.MediaPlayer2.Player.xml
@@ -1,0 +1,108 @@
+<?xml version="1.0" ?>
+<node>
+  <interface name="org.mpris.MediaPlayer2.Player">
+    <method name="Next" tp:name-for-bindings="Next">
+    </method>
+
+    <method name="Previous" tp:name-for-bindings="Previous">
+    </method>
+
+    <method name="Pause" tp:name-for-bindings="Pause">
+    </method>
+
+    <method name="PlayPause" tp:name-for-bindings="PlayPause">
+    </method>
+
+    <method name="Stop" tp:name-for-bindings="Stop">
+    </method>
+
+    <method name="Play" tp:name-for-bindings="Play">
+    </method>
+
+    <method name="Seek" tp:name-for-bindings="Seek">
+      <arg direction="in" type="x" name="Offset" tp:type="Time_In_Us">
+      </arg>
+    </method>
+
+    <method name="SetPosition" tp:name-for-bindings="Set_Position">
+      <arg direction="in" type="o" tp:type="Track_Id" name="TrackId">
+      </arg>
+      <arg direction="in" type="x" tp:type="Time_In_Us" name="Position">
+      </arg>
+    </method>
+
+    <method name="OpenUri" tp:name-for-bindings="Open_Uri">
+      <arg direction="in" type="s" tp:type="Uri" name="Uri">
+      </arg>
+    </method>
+
+    <property name="PlaybackStatus" tp:name-for-bindings="Playback_Status" type="s" tp:type="Playback_Status" access="read">
+      <annotation name="org.freedesktop.DBus.Property.EmitsChangedSignal" value="true"/>
+    </property>
+
+    <property name="LoopStatus" type="s" access="readwrite"
+              tp:name-for-bindings="Loop_Status" tp:type="Loop_Status">
+      <annotation name="org.freedesktop.DBus.Property.EmitsChangedSignal" value="true"/>
+    </property>
+
+    <property name="Rate" tp:name-for-bindings="Rate" type="d" tp:type="Playback_Rate" access="readwrite">
+      <annotation name="org.freedesktop.DBus.Property.EmitsChangedSignal" value="true"/>
+    </property>
+
+    <property name="Shuffle" tp:name-for-bindings="Shuffle" type="b" access="readwrite">
+      <annotation name="org.freedesktop.DBus.Property.EmitsChangedSignal" value="true"/>
+    </property>
+
+    <property name="Metadata" tp:name-for-bindings="Metadata" type="a{sv}" tp:type="Metadata_Map" access="read">
+      <annotation name="org.freedesktop.DBus.Property.EmitsChangedSignal" value="true"/>
+      <annotation name="org.qtproject.QtDBus.QtTypeName" value="QVariantMap"/>
+    </property>
+
+    <property name="Volume" type="d" tp:type="Volume" tp:name-for-bindings="Volume" access="readwrite">
+      <annotation name="org.freedesktop.DBus.Property.EmitsChangedSignal" value="true" />
+    </property>
+
+    <property name="Position" type="x" tp:type="Time_In_Us" tp:name-for-bindings="Position" access="read">
+        <annotation name="org.freedesktop.DBus.Property.EmitsChangedSignal" value="false"/>
+    </property>
+
+    <property name="MinimumRate" tp:name-for-bindings="Minimum_Rate" type="d" tp:type="Playback_Rate" access="read">
+      <annotation name="org.freedesktop.DBus.Property.EmitsChangedSignal" value="true"/>
+    </property>
+
+    <property name="MaximumRate" tp:name-for-bindings="Maximum_Rate" type="d" tp:type="Playback_Rate" access="read">
+      <annotation name="org.freedesktop.DBus.Property.EmitsChangedSignal" value="true"/>
+    </property>
+
+    <property name="CanGoNext" tp:name-for-bindings="Can_Go_Next" type="b" access="read">
+      <annotation name="org.freedesktop.DBus.Property.EmitsChangedSignal" value="true"/>
+    </property>
+
+    <property name="CanGoPrevious" tp:name-for-bindings="Can_Go_Previous" type="b" access="read">
+      <annotation name="org.freedesktop.DBus.Property.EmitsChangedSignal" value="true"/>
+    </property>
+
+    <property name="CanPlay" tp:name-for-bindings="Can_Play" type="b" access="read">
+      <annotation name="org.freedesktop.DBus.Property.EmitsChangedSignal" value="true"/>
+    </property>
+
+    <property name="CanPause" tp:name-for-bindings="Can_Pause" type="b" access="read">
+      <annotation name="org.freedesktop.DBus.Property.EmitsChangedSignal" value="true"/>
+    </property>
+
+    <property name="CanSeek" tp:name-for-bindings="Can_Seek" type="b" access="read">
+      <annotation name="org.freedesktop.DBus.Property.EmitsChangedSignal" value="true"/>
+    </property>
+
+    <property name="CanControl" tp:name-for-bindings="Can_Control" type="b" access="read">
+      <annotation name="org.freedesktop.DBus.Property.EmitsChangedSignal" value="false"/>
+    </property>
+
+    <signal name="Seeked" tp:name-for-bindings="Seeked">
+      <arg name="Position" type="x" tp:type="Time_In_Us">
+      </arg>
+    </signal>
+
+  </interface>
+</node>
+<!-- vim:set sw=2 sts=2 et ft=xml: -->

plasma/generic/dataengines/mpris2/org.mpris.MediaPlayer2.xml
@@ -1,0 +1,41 @@
+<?xml version="1.0" ?>
+<node>
+  <interface name="org.mpris.MediaPlayer2">
+    <annotation name="org.freedesktop.DBus.Property.EmitsChangedSignal" value="true"/>
+
+    <method name="Raise" tp:name-for-bindings="Raise">
+    </method>
+
+    <method name="Quit" tp:name-for-bindings="Quit">
+    </method>
+
+    <property name="CanQuit" type="b" tp:name-for-bindings="Can_Quit" access="read">
+    </property>
+
+    <property name="Fullscreen" type="b" tp:name-for-bindings="Fullscreen" access="readwrite">
+    </property>
+
+    <property name="CanSetFullscreen" type="b" tp:name-for-bindings="Can_Set_Fullscreen" access="read">
+    </property>
+
+    <property name="CanRaise" type="b" tp:name-for-bindings="Can_Raise" access="read">
+    </property>
+
+    <property name="HasTrackList" type="b" tp:name-for-bindings="Has_TrackList" access="read">
+    </property>
+
+    <property name="Identity" type="s" tp:name-for-bindings="Identity" access="read">
+    </property>
+
+    <property name="DesktopEntry" type="s" tp:name-for-bindings="Desktop_Entry" access="read">
+    </property>
+
+    <property name="SupportedUriSchemes" type="as" tp:name-for-bindings="Supported_Uri_Schemes" access="read">
+    </property>
+
+    <property name="SupportedMimeTypes" type="as" tp:name-for-bindings="Supported_Mime_Types" access="read">
+    </property>
+
+  </interface>
+</node>
+<!-- vim:set sw=2 sts=2 et ft=xml: -->

plasma/generic/dataengines/mpris2/playeractionjob.cpp
@@ -21,7 +21,10 @@
 
 #include "playercontrol.h"
 
-#include <QDBusInterface>
+#include <dbusproperties.h>
+#include <mprisplayer.h>
+#include <mprisroot.h>
+
 #include <QDBusPendingCallWatcher>
 #include <QDBusPendingReply>
 #include <QDBusVariant>
@@ -59,7 +62,7 @@ void PlayerActionJob::start()
         listenToCall(m_controller->playerInterface()->asyncCall(operation));
     } else if (operation == "Seek") {
         if (parameters().value("microseconds").type() == QVariant::LongLong) {
-            listenToCall(m_controller->playerInterface()->asyncCall(operation, parameters()["microseconds"]));
+            listenToCall(m_controller->playerInterface()->Seek(parameters()["microseconds"].toLongLong()));
         } else {
             setErrorText("microseconds");
             setError(MissingArgument);
@@ -67,9 +70,9 @@ void PlayerActionJob::start()
         }
     } else if (operation == "SetPosition") {
         if (parameters().value("microseconds").type() == QVariant::LongLong) {
-            listenToCall(m_controller->playerInterface()->asyncCall(operation,
-                         m_controller->trackId(),
-                         parameters()["microseconds"]));
+            listenToCall(m_controller->playerInterface()->SetPosition(
+                             m_controller->trackId(),
+                             parameters()["microseconds"].toLongLong()));
         } else {
             setErrorText("microseconds");
             setError(MissingArgument);
@@ -77,8 +80,8 @@ void PlayerActionJob::start()
         }
     } else if (operation == "OpenUri") {
         if (parameters().value("uri").canConvert<KUrl>()) {
-            listenToCall(m_controller->playerInterface()->asyncCall(operation,
-                         QString::fromLatin1(parameters()["uri"].toUrl().toEncoded())));
+            listenToCall(m_controller->playerInterface()->OpenUri(
+                             QString::fromLatin1(parameters()["uri"].toUrl().toEncoded())));
         } else {
             kDebug() << "uri was of type" << parameters().value("uri").userType();
             setErrorText("uri");
@@ -153,7 +156,7 @@ void PlayerActionJob::callFinished(QDBusPendingCallWatcher* watcher)
 void PlayerActionJob::setDBusProperty(const QString& iface, const QString& propName, const QDBusVariant& value)
 {
     listenToCall(
-        m_controller->propertiesInterface()->asyncCall("Set", iface, propName, QVariant::fromValue<QDBusVariant>(value))
+        m_controller->propertiesInterface()->Set(iface, propName, value)
     );
 }
 

commit a9ba915a838139aff1eef827ceb633fb8bb0b11a

Fix signal connection in mpris2 dataengine
moc can't tell that qlonglong and qint64 are the same type, so the
connection failed, breaking seek notifications.

Fixes problem introduced by commit
f443382388363a6e5b8e12e7154f0fabdbe54e8a

plasma/generic/dataengines/mpris2/playercontainer.cpp
@@ -117,7 +117,7 @@ PlayerContainer::PlayerContainer(const QString& busAddress, QObject* parent)
             this,         SLOT(propertiesChanged(QString,QVariantMap,QStringList)));
 
     connect(m_playerIface, SIGNAL(Seeked(qlonglong)),
-            this,          SLOT(seeked(qint64)));
+            this,          SLOT(seeked(qlonglong)));
 
     refresh();
 }
@@ -365,7 +365,7 @@ void PlayerContainer::propertiesChanged(
     checkForUpdate();
 }
 
-void PlayerContainer::seeked(qint64 position)
+void PlayerContainer::seeked(qlonglong position)
 {
     setData("Position", position);
     setData(POS_UPD_STRING, QDateTime::currentDateTimeUtc());

plasma/generic/dataengines/mpris2/playercontainer.h
@@ -77,7 +77,7 @@ private slots:
     void propertiesChanged(const QString& interface,
                            const QVariantMap& changedProperties,
                            const QStringList& invalidatedProperties);
-    void seeked(qint64 position);
+    void seeked(qlonglong position);
 
 private:
     void copyProperty(const QString& propName, const QVariant& value, QVariant::Type expType, UpdateType updType);