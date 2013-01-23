4fbbb072a998778ca93ef5c4136a9a5add4893ea
Weng Xuetian <wengxt@gmail.com>
2013-01-23 14:14:13 -0500

    extract some missing translation in javascript
    REVIEW: 108534

--- plasma/generic/applets/lock_logout/Messages.sh
+++ plasma/generic/applets/lock_logout/Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
-$XGETTEXT `find . -name \*.qml -o -name \*.cpp` -o $podir/plasma_applet_lockout.pot
+$XGETTEXT `find . -name \*.js -name \*.qml -o -name \*.cpp` -o $podir/plasma_applet_lockout.pot
 rm -f rc.cpp
