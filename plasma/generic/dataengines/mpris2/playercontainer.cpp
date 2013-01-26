KDE/kde-workspace

commit 10216c7f531810a63d54e567cb223e1e0d9da93a

Fix handling of encoded URIs in mpris2 engine
A couple of entries in the metadata property of the MPRIS2 interface are
sent as encoded URIs.  We decode these in the mpris2 engine, replacing
the QStrings with QUrls that can be used easily by the consumer of the
engine.

This fixes the display of album artwork in the Now Playing widget when
mpris:artUrl contains %-encoded non-ASCII characters.

BUG: 303088
FIXED-IN: 4.10

plasma/generic/dataengines/mpris2/playercontainer.cpp
@@ -150,6 +150,26 @@ void PlayerContainer::refresh()
     ++m_fetchesPending;
 }
 
+static bool decodeUri(QVariantMap &map, const QString& entry) {
+    if (map.contains(entry)) {
+        QString urlString = map.value(entry).toString();
+        QUrl url = QUrl::fromEncoded(urlString.toAscii());
+        if (!url.isValid()) {
+            // try to be lenient
+            url = QUrl(urlString);
+        }
+        if (url.isValid()) {
+            map.insert(entry, QVariant(url));
+            return true;
+        } else {
+            map.remove(QLatin1String("mpris:artUrl"));
+            return false;
+        }
+    }
+    // count it as a success if it doesn't exist
+    return true;
+}
+
 void PlayerContainer::copyProperty(const QString& propName, const QVariant& _value, QVariant::Type expType, UpdateType updType)
 {
     QVariant value = _value;
@@ -162,9 +182,17 @@ void PlayerContainer::copyProperty(const QString& propName, const QVariant& _val
                     << "with the wrong type; it should be D-Bus type \"a{sv}\"";
                 return;
             }
-            QVariantMap metadata;
-            arg >> metadata;
-            value = QVariant(metadata);
+            QVariantMap map;
+            arg >> map;
+            if (propName == QLatin1String("Metadata")) {
+                if (!decodeUri(map, QLatin1String("mpris:artUrl"))) {
+                    kWarning() << m_dbusAddress << "has an invalid URL for the mpris:artUrl entry of the \"Metadata\" property";
+                }
+                if (!decodeUri(map, QLatin1String("xesam:url"))) {
+                    kWarning() << m_dbusAddress << "has an invalid URL for the xesam:url entry of the \"Metadata\" property";
+                }
+            }
+            value = QVariant(map);
         }
     }
     if (value.type() != expType) {

commit 29bc3ff09abce222c8edc87721c02cae6715628b

mpris2: do not remove the wrong metadata entry when URI decoding fails
If decoding any URI in the metadata map fails, we used to remove the
mpris:artUrl entry instead of the entry that failed to decode.  This
fixes that behaviour.

This problem was introduced in commit
10216c7f531810a63d54e567cb223e1e0d9da93a

plasma/generic/dataengines/mpris2/playercontainer.cpp
@@ -162,7 +162,7 @@ static bool decodeUri(QVariantMap &map, const QString& entry) {
             map.insert(entry, QVariant(url));
             return true;
         } else {
-            map.remove(QLatin1String("mpris:artUrl"));
+            map.remove(entry);
             return false;
         }
     }