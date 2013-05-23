882534d76e9d467bd16cb38c6c2e8354aa388cc3
Chusslove Illich (Часлав Илић) <caslav.ilic@gmx.net>
2013-05-23 15:31:59 +0200

    Do not forget to include messages from rc.cpp. (cherry picked from commit c53b980107a0f3bdfbfca8d925e9a834f71fd515)

--- plasma/generic/applets/notifications/Messages.sh
+++ plasma/generic/applets/notifications/Messages.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
 $XGETTEXT `find . -name \*.qml` -L Java -o $podir/plasma_applet_org.kde.notifications.pot
+$XGETTEXT rc.cpp -jo $podir/plasma_applet_org.kde.notifications.pot
 rm -f rc.cpp
