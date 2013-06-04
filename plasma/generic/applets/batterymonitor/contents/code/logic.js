95d03dc7ee1d0c981add17962e87e24da91eca40
Kai Uwe Broulik <kde@privat.broulik.de>
2013-06-05 00:11:43 +0200

    Also handle not present batteries in the tooltip. CCBUG: 320678

--- plasma/generic/applets/batterymonitor/contents/code/logic.js
+++ plasma/generic/applets/batterymonitor/contents/code/logic.js
@@ -159,7 +159,13 @@ function updateTooltip() {
             var b = batteries.get(i);
             text += "<tr>";
             text += "<td align='right'>" + i18nc("Placeholder is battery name", "%1:", b["Pretty Name"]) + " </td>";
-            text += "<td>" + i18nc("Placeholder is battery percentage", "%1%", b["Percent"]) + "</td>";
+            text += "<td>";
+            if (b["Plugged in"]) {
+                text += i18nc("Placeholder is battery percentage", "%1%", b["Percent"]);
+            } else {
+                text += i18n("N/A")
+            }
+            text += "</td>";
             text += "</tr>";
 
             if (b["Is Power Supply"]) { hasPowerSupply = true; }
