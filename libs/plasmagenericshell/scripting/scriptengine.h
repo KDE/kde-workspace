e1d4702f60e807bb33556eba7260a03b8eb51462
Aaron Seigo <aseigo@kde.org>
2013-06-05 14:48:20 +0200

    add ConfigFile to Plasma Desktop Scripting
    borrowed and improved from active-settings

--- libs/plasmagenericshell/scripting/scriptengine.h
+++ libs/plasmagenericshell/scripting/scriptengine.h
@@ -83,6 +83,7 @@ private:
     static QScriptValue applicationPath(QScriptContext *context, QScriptEngine *engine);
     static QScriptValue userDataPath(QScriptContext *context, QScriptEngine *engine);
     static QScriptValue knownWallpaperPlugins(QScriptContext *context, QScriptEngine *engine);
+    static QScriptValue configFile(QScriptContext *context, QScriptEngine *engine);
 
     // helpers
     static QScriptValue createContainment(const QString &type, const QString &defautPlugin,
@@ -96,7 +97,7 @@ private:
     QScriptValue m_scriptSelf;
 };
 
-static const int PLASMA_DESKTOP_SCRIPTING_VERSION = 5;
+static const int PLASMA_DESKTOP_SCRIPTING_VERSION = 6;
 }
 
 #endif
