3aadf8b75ebe9b98346d683d258a1b9394e6cf9b
David Edmundson <kde@davidedmundson.co.uk>
2013-08-26 22:06:25 +0100

    Kickoff: Re-enable runners model

--- plasma/desktop/applets/kickoff/core/kickoffplugin.cpp
+++ plasma/desktop/applets/kickoff/core/kickoffplugin.cpp
@@ -33,7 +33,7 @@ void KickoffPlugin::registerTypes (const char *uri)
     Q_ASSERT(uri == QLatin1String("org.kde.plasma.private.kickoff"));
     qmlRegisterType<Kickoff::ApplicationModel>(uri, 0, 1, "ApplicationModel");
     qmlRegisterType<Kickoff::FavoritesModel>(uri, 0, 1, "FavoritesModel");
-//     qmlRegisterType<Kickoff::KRunnerModel>(uri, 0, 1, "KRunnerModel");
+    qmlRegisterType<Kickoff::KRunnerModel>(uri, 0, 1, "KRunnerModel");
     qmlRegisterType<Kickoff::LeaveModel>(uri, 0, 1, "LeaveModel");
     qmlRegisterType<Kickoff::RecentlyUsedModel>(uri, 0, 1, "RecentlyUsedModel");
     qmlRegisterType<Kickoff::SystemModel>(uri, 0, 1, "SystemModel");
