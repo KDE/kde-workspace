46f89e025889edca81b3503ab7e90e0e4791e4f5
David Edmundson <kde@davidedmundson.co.uk>
2013-10-21 04:24:20 +0200

    Screenlocker: Don't build unused Greeter proxy widget

--- ksmserver/screenlocker/greeter/greeterapp.cpp
+++ ksmserver/screenlocker/greeter/greeterapp.cpp
@@ -20,7 +20,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 *********************************************************************/
 #include "greeterapp.h"
 #include "kscreensaversettings.h"
-#include "greeter.h"
 #include "sessions.h"
 #include "screensaverwindow.h"
 
