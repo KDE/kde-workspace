7b65db3faabe26c12b0914b124a7bd0db31e1d01
Sebastian Kügler <sebas@kde.org>
2013-12-02 15:48:00 +0100

    Revert 4540dc32f08906574
    That patch was just papering over a big in KWindowSystem, which in the
    meantime has been fixed. Reverting this patch makes the taskbar react
    directly to clicks, and not wait until the mouse has moved.
    
    Suggested-by: Eike Hein

--- plasma/desktop/applets/taskmanager/plugin/backend.cpp
+++ plasma/desktop/applets/taskmanager/plugin/backend.cpp
@@ -142,6 +142,10 @@ void Backend::activateItem(int id, bool toggle)
         } else {
             taskItem->task()->activate();
         }
+
+        //FIXME TODO HACK This papers over XSendEvent() calls lacking an explicit flush in
+        //KWindowSystem; not addressed there due to pending xcb port.
+        XFlush(QX11Info::display());
     } else if (item->itemType() == TaskManager::LauncherItemType) {
         static_cast<TaskManager::LauncherItem*>(item)->launch();
     }
