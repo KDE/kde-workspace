4598f3c1dae7ab5e6fcf0a6f49c4d974abc9f9ff
Àlex Fiestas <afiestas@kde.org>
2013-12-05 15:26:23 +0100

    [startkde] use the qtpaths found in $PATH

--- startkde.cmake
+++ startkde.cmake
@@ -19,7 +19,7 @@ unset DYLD_FORCE_FLAT_NAMESPACE
 # in case we have been started with full pathname spec without being in PATH
 bindir=`echo "$0" | sed -n 's,^\(/.*\)/[^/][^/]*$,\1,p'`
 if [ -n "$bindir" ]; then
-  qbindir=`$bindir/qtpaths --binaries-dir`
+  qbindir=`qtpaths --binaries-dir`
   qdbus=$qbindir/qdbus
   case $PATH in
     $bindir|$bindir:*|*:$bindir|*:$bindir:*) ;;
