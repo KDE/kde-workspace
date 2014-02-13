2b5cfde6ef79d9ceefcb18e3c06aaabc93184421
Sebastian Kügler <sebas@kde.org>
2014-02-13 04:47:46 +0100

    Complete license headers instead of shortened version

--- plasma/generic/wallpapers/image/image.cpp
+++ plasma/generic/wallpapers/image/image.cpp
@@ -1,14 +1,25 @@
-/*
-  Copyright (c) 2007 by Paolo Capriotti <p.capriotti@gmail.com>
-  Copyright (c) 2007 by Aaron Seigo <aseigo@kde.org>
-  Copyright (c) 2008 by Alexis Ménard <darktears31@gmail.com>
-  Copyright (c) 2008 by Petri Damsten <damu@iki.fi>
-
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
-*/
+/***************************************************************************
+ *   Copyright 2007 Paolo Capriotti <p.capriotti@gmail.com>                *
+ *   Copyright 2007 Aaron Seigo <aseigo@kde.org>                           *
+ *   Copyright 2008 Petri Damsten <damu@iki.fi>                            *
+ *   Copyright 2008 Alexis Ménard <darktears31@gmail.com>                  *
+ *   Copyright 2014 Sebastian Kügler <sebas@kde.org>                       *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .        *
+ ***************************************************************************/
 
 #include "image.h"
 #include "wallpaperpackage.h"
