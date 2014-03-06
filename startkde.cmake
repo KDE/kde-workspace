2aa59744b89309945ec991c014fd3375a3d8bdc6
Sebastian Kügler <sebas@kde.org>
2014-03-06 01:51:11 +0100

    Make sure the Oxygen font is found
    Usually, with install prefix set to /usr, the installed oxygen font is
    found automatically by fontconfig. If we're installed to a different
    prefix, we need to point fontconfig at the font. We do that by linking
    it from either XDG_DATA_HOME or ~/.fonts/ and updating fontconfig with
    it.

--- startkde.cmake
+++ startkde.cmake
@@ -82,15 +82,47 @@ cat >$kdeglobalsfile <<EOF
 XftAntialias=true
 XftHintStyle=hintmedium
 XftSubPixel=none
-desktopFont=Oxygen,12,-1,5,50,0,0,0,0,0
+desktopFont=Oxygen-Sans,12,-1,5,50,0,0,0,0,0
 fixed=Oxygen Mono,11,-1,5,50,0,0,0,0,0
-font=Oxygen,12,-1,5,50,0,0,0,0,0
-menuFont=Oxygen,11,-1,5,50,0,0,0,0,0
-smallestReadableFont=Oxygen,10,-1,5,50,0,0,0,0,0
-taskbarFont=Oxygen,11,-1,5,50,0,0,0,0,0
-toolBarFont=Oxygen,10,-1,5,50,0,0,0,0,0
+font=Oxygen-Sans,12,-1,5,50,0,0,0,0,0
+menuFont=Oxygen-Sans,11,-1,5,50,0,0,0,0,0
+smallestReadableFont=Oxygen-Sans,10,-1,5,50,0,0,0,0,0
+taskbarFont=Oxygen-Sans,11,-1,5,50,0,0,0,0,0
+toolBarFont=Oxygen-Sans,10,-1,5,50,0,0,0,0,0
 EOF
 }
+
+# Make sure the Oxygen font is installed
+# This is necessary for setups where CMAKE_INSTALL_PREFIX
+# is not in /usr. fontconfig looks in /usr, ~/.fonts and
+# $XDG_DATA_HOME for fonts. In this case, we symlink the
+# Oxygen font under ${XDG_DATA_HOME} and make it known to
+# fontconfig
+
+usr_share="/usr/share"
+install_share="@CMAKE_INSTALL_PREFIX@/@SHARE_INSTALL_PREFIX@"
+
+if [ ! $install_share = $usr_share ]; then
+
+    if [ ${XDG_DATA_HOME} ]; then
+        fontsDir="${XDG_DATA_HOME}/fonts"
+    else
+        fontsDir="${HOME}/.fonts"
+    fi
+
+    test -d $fontsDir || {
+        mkdir -p $fontsDir
+    }
+
+    oxygenDir=$fontsDir/oxygen
+    prefixDir="@CMAKE_INSTALL_PREFIX@/@SHARE_INSTALL_PREFIX@/fonts/oxygen"
+
+    # if the oxygen dir doesn't exist, create a symlink to be sure that the
+    # Oxygen font is available to the user
+    test -d $oxygenDir || test -d $prefixDir && {
+        test -h $oxygenDir || ln -s $prefixDir $oxygenDir && fc-cache $oxygenDir
+    }
+fi
 
 kstartupconfig5
 returncode=$?
