970b64f84ace1dd2c44de654be59a8ccd1fdc5b1
Marco Martin <notmart@gmail.com>
2014-03-19 19:14:58 +0100

    remove references to the export

--- plasma-workspace/krunner/shellpluginloader.h
+++ plasma-workspace/krunner/shellpluginloader.h
@@ -22,9 +22,7 @@
 
 #include <Plasma/PluginLoader>
 
-#include <plasmaquick/plasmaquick_export.h>
-
-class PLASMAQUICK_EXPORT ShellPluginLoader : public Plasma::PluginLoader
+class ShellPluginLoader : public Plasma::PluginLoader
 {
 public:
     ShellPluginLoader();
