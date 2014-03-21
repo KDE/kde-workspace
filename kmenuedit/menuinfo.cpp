ef0316e81f8a025851c1db8e9615b853f1c85f9d
Àlex Fiestas <afiestas@kde.org>
2014-03-21 13:12:45 +0100

    s/ifndef/ifdef/

--- kmenuedit/menuinfo.cpp
+++ kmenuedit/menuinfo.cpp
@@ -177,7 +177,7 @@ void MenuFolderInfo::save(MenuFile *menuFile)
 {
    if (s_deletedApps)
    {
-#ifndef WITH_HOTKEYS
+#ifdef WITH_HOTKEYS
       // Remove hotkeys for applications that have been deleted
       for(QStringList::ConstIterator it = s_deletedApps->constBegin();
           it != s_deletedApps->constEnd(); ++it)
